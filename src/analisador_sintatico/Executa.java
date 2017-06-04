45f4dc0a410125ff744b5b3c0536887b3af62b5e
Marcia <rcsilva.marcia@gmail.com>
2017-06-04 09:46:49 -0300

    Alterações q podem mudar o mundo

---
+++ src/analisador_sintatico/Executa.java
@@ -0,0 +1,232 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+package analisador_sintatico;
+///**
+// *
+// * @author Regina
+// */
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Executa extends javax.swing.JFrame {
+
+    public JTextArea jText;
+    public JTextField carregaArquivo;
+    private Parser p;
+
+    public void executar() throws Exception {
+        jText = jTextAreaInsert;
+        try {
+            p = new Parser(new analisador_sintatico.Lexer(new java.io.FileInputStream("sentenca.txt")));
+            p.parse();
+
+            jTextArea1.setText("Sentença correta");
+
+        } catch (Exception e) {
+            System.err.println(e);
+            jTextArea1.setText(p.getErro());
+        }
+    }
+
+    public Executa() {
+        initComponents();
+        super.getContentPane().setBackground(new Color(136, 183, 201));
+        super.setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        carregaArquivo = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextAreaInsert = new javax.swing.JTextArea();
+        jButton4 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+
+        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18));
+        jLabel1.setText("Compilador - Análise Sintática");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jLabel2.setText("Insira aqui o código: ");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jButton1.setText("Analisar");
+        jButton1.setPreferredSize(new java.awt.Dimension(109, 25));
+        jButton1.addActionListener((java.awt.event.ActionEvent evt) -> {
+            jButton1ActionPerformed();
+        });
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jButton2.setText("Limpar");
+        jButton2.addActionListener((java.awt.event.ActionEvent evt) -> {
+            jButton2ActionPerformed();
+        });
+//        jButton2.addActionListener(new java.awt.event.ActionListener() {
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                jButton2ActionPerformed();
+//            }
+//        });
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jLabel3.setText("Resultado da Análise: ");
+
+        jTextAreaInsert.setEditable(true);
+        jTextAreaInsert.setColumns(20);
+        jTextAreaInsert.setRows(5);
+        jScrollPane2.setViewportView(jTextAreaInsert);
+
+        jButton4.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jButton4.setText("Limpar");
+        jButton4.addActionListener((java.awt.event.ActionEvent evt) -> {
+            jButton4ActionPerformed();
+        });
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jButton3.setText("Limpar Tudo");
+        jButton3.addActionListener((java.awt.event.ActionEvent evt) -> {
+            jButton3ActionPerformed();
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel2)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jButton2))
+                                .addComponent(jScrollPane1)
+                                .addComponent(jScrollPane2)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addGroup(layout.createSequentialGroup()
+                                                        .addComponent(jLabel3)
+                                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                        .addGap(0, 169, Short.MAX_VALUE)
+                                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addGap(18, 18, 18)
+                                                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addGap(70, 70, 70)))
+                                        .addComponent(jButton4)))
+                        .addContainerGap())
+                .addGroup(layout.createSequentialGroup()
+                        .addGap(185, 185, 185)
+                        .addComponent(jLabel1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addGap(42, 42, 42)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                .addComponent(jButton2)
+                                                .addComponent(jLabel2))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jLabel3))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addContainerGap()
+                                        .addComponent(jLabel1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jButton4)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void jButton1ActionPerformed() {
+        try {
+            try (java.io.PrintWriter writer = new java.io.PrintWriter("sentenca.txt", "UTF-8")) {
+                writer.print(jTextAreaInsert.getText());
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            }
+            executar();
+        } catch (Exception e) {
+        }
+    }
+
+    private void jButton2ActionPerformed() {
+        jTextAreaInsert.setText("");
+    }
+
+    private void jButton3ActionPerformed() {
+        jButton2ActionPerformed();
+        jButton4ActionPerformed();
+    }
+
+    private void jButton4ActionPerformed() {
+        jTextArea1.setText("");
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Executa.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
+        }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Executa().setVisible(true);
+        });
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Executa().setVisible(true);
+//            }
+//        });
+    }
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextAreaInsert;
+}
